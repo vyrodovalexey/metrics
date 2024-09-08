2ddfeba0d7271352ca1800491beb65d2b66f2782
alexey <vyrodov.alexey@gmail.com>
2024-09-08 16:37:19 +0300

    server release

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"github.com/vyrodovalexey/metrics/internal/handlers"
+	"github.com/vyrodovalexey/metrics/internal/storage"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	gauge := make(map[string]storage.Gauge)
+	counter := make(map[string][]storage.Counter)
+	mst := storage.MemStorage{GaugeMap: gauge, CounterMap: counter}
+
+	http.HandleFunc("/update/", handlers.Update(&mst))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
