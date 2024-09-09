00d6972d68220d63d78e36beef47921b15b34bf0
alexey <vyrodov.alexey@gmail.com>
2024-09-09 20:33:28 +0300

    fix Counter type

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 
 	gauge := make(map[string]storage.Gauge)
-	counter := make(map[string][]storage.Counter)
+	counter := make(map[string]storage.Counter)
 	mst := storage.MemStorage{GaugeMap: gauge, CounterMap: counter}
 
 	http.HandleFunc("/update/", handlers.Update(&mst))
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -9,13 +9,13 @@ type Counter = int64
 
 type MemStorage struct {
 	GaugeMap   map[string]Gauge
-	CounterMap map[string][]Counter
+	CounterMap map[string]Counter
 }
 
 func (storage *MemStorage) AddCounter(name string, item string) error {
 	counter, err := strconv.ParseInt(item, 10, 64)
 	if err == nil {
-		storage.CounterMap[name] = append(storage.CounterMap[name], counter)
+		storage.CounterMap[name] = storage.CounterMap[name] + counter
 	}
 	return err
 
