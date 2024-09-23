d3eef3c117ba613fa4fdd20747c3917b675fc0fc
alexey <vyrodov.alexey@gmail.com>
2024-09-23 22:25:51 +0300

    add GetJson

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,15 +1,51 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/vyrodovalexey/metrics/internal/storage"
 	"net/http"
 )
 
+type Metrics struct {
+	ID    string   `json:"id"`              // имя метрики
+	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
+	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
+}
+
 const (
 	badrequest = "Bad Request"
 )
+
+func UpdateJson(st storage.Storage) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		switch c.Param("type") {
+		case "gauge":
+			err := st.AddGauge(c.Param("name"), c.Param("value"))
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": badrequest,
+				})
+			}
+
+		case "counter":
+			err := st.AddCounter(c.Param("name"), c.Param("value"))
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": badrequest,
+				})
+			}
+		default:
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": badrequest,
+			})
+			return
+		}
+	}
+}
 
 func Update(st storage.Storage) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -70,6 +106,64 @@ func Get(st storage.Storage) gin.HandlerFunc {
 				"error": badrequest,
 			})
 			return
+		}
+	}
+}
+
+func GetJson(st storage.Storage) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		if c.Request.Header.Get("Content-Type") != "application/json" {
+			c.JSON(http.StatusUnsupportedMediaType, gin.H{
+				"error": badrequest,
+			})
+			return
+		} else {
+			var metrics Metrics
+			err := json.NewDecoder(c.Request.Body).Decode(&metrics)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": badrequest,
+				})
+				return
+			}
+			switch metrics.MType {
+			case "gauge":
+				g, e := st.GetGauge(metrics.ID)
+				if !e {
+					c.JSON(http.StatusNotFound, gin.H{
+						"error": badrequest,
+					})
+					return
+				} else {
+					gs := fmt.Sprintf("%v", g)
+					c.JSON(http.StatusOK, gin.H{
+						"id":    metrics.ID,
+						"type":  metrics.MType,
+						"value": gs,
+					})
+				}
+			case "counter":
+				g, e := st.GetCounter(metrics.ID)
+				if !e {
+					c.JSON(http.StatusNotFound, gin.H{
+						"error": badrequest,
+					})
+					return
+				} else {
+					gs := fmt.Sprintf("%v", g)
+					c.JSON(http.StatusOK, gin.H{
+						"id":    metrics.ID,
+						"type":  metrics.MType,
+						"delta": gs,
+					})
+				}
+			default:
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": badrequest,
+				})
+				return
+			}
 		}
 	}
 }
