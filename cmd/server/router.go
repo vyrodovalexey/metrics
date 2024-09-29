339ec4bc35aee764cea89207c9a41cdefc69eb48
alexey <vyrodov.alexey@gmail.com>
2024-09-29 12:59:34 +0300

    init iter8

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-gonic/contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"github.com/vyrodovalexey/metrics/internal/handlers"
 	"github.com/vyrodovalexey/metrics/internal/storage"
@@ -13,6 +14,7 @@ func SetupRouter(st storage.Storage, log *zap.SugaredLogger) *gin.Engine {
 	gin.DefaultWriter = io.Discard
 	router := gin.Default()
 	router.Use(LoggingMiddleware(log))
+	router.Use(gzip.Gzip(gzip.DefaultCompression))
 	router.POST("/update/:type/:name/:value", handlers.Update(st))
 	router.GET("/value/:type/:name", handlers.Get(st))
 	router.POST("/update/", handlers.UpdateJSON(st))
